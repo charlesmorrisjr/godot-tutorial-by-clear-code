5d8fb7b7dd13ea8ab191e3eafdf7858cb3d47936
Charles Morris Jr <cmorrisjr777@gmail.com>
2026-01-27 01:02:55 -0500

    Frogger basic movement
    - Added map
    - Added house and tree scene
    - Created player with movement

---
+++ frogger/scenes/player.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var direction: Vector2 = Vector2.ZERO
+var speed: int = 2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(_delta: float) -> void:
+	direction = Input.get_vector("left", "right", "up", "down")
+	position += direction * speed
